eb065ad93596d68257ceec2a5628fa95d37d3ab2
Prathmesh6968 <prathmeshcoder69@gmail.com>
2025-11-29 15:45:50 +0530

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,42 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Navigation } from "@/components/navigation";
+import Home from "@/pages/home";
+import Browse from "@/pages/browse";
+import Watchlist from "@/pages/watchlist";
+import AnimeDetail from "@/pages/anime-detail";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/browse" component={Browse} />
+      <Route path="/watchlist" component={Watchlist} />
+      <Route path="/anime/:id" component={AnimeDetail} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="animestream-theme">
+        <TooltipProvider>
+          <div className="min-h-screen bg-background text-foreground">
+            <Navigation />
+            <Router />
+          </div>
+          <Toaster />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
